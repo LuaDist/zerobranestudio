d6ad913e66ce027d7107f26d7528e7ec80a29fc6
Peter Drahoš <drahosp@gmail.com>
2013-04-02 15:00:55 +0300

    Update zbstudio.lua

--- zbstudio.lua
+++ zbstudio.lua
@@ -1,5 +1,11 @@
 local lfs = require "lfs"
 
-local share = _PROGDIR .. "/../share/zerobranestudio"
+-- Get LuaDist deployment path
+local paths = {}
+package.path:gsub("([^;]+)", function(c) table.insert(paths, c) end)
+local path = paths[2]:gsub("[/\\]bin[/\\]%.%..*","")
+
+local share = path .. "/share/zerobranestudio"
+
 lfs.chdir(share)
 assert(loadfile("src/main.lua"))("zbstudio")
